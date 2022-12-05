54ae6db7cf98e658a5de63325b62fb260606ab05
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-12-05 17:24:55 +0100

    #14 - Implemented the Contact and Wallet page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { Fragment, useRef } from 'react'
+import CommonSection from '../components/UI/CommonSection'
+import { Container, Row, Col } from 'reactstrap'
+import Helmet from '../components/Utils/Helmet'
+
 
 const Contact = () => {
+
+    const nameRef = useRef('')
+    const emailRef = useRef('')
+    const subjectRef = useRef('')
+    const messageRef = useRef('')
+
+    const handlerSubmit = (e) => {
+        // Preventing refresh
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            Contact
-        </div>
+        /* Helmet */
+        <Helmet title='Contact'>
+            <Fragment>
+                {/* CommonSection */}
+                <CommonSection title='Contact' />
+                <section>
+                    <Container>
+                        <Row>
+                            {/* lg='6' md='6' */}
+                            <Col lg='6' md='6' className="m-auto text-center">
+                                <h2>Drop a Message</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere eros sed sagittis pellentesque. Donec dapibus lectus turpis, vel facilisis.
+                                </p>
+                                {/* contact */}
+                                <div className="contact mt-4">
+                                    <form onSubmit={handlerSubmit}>
+                                        {/* form_input */}
+                                        <div className="form_input">
+                                            <input type="text" placeholder='Enter your name' ref={nameRef} />
+                                        </div>
+                                        {/* form_input */}
+                                        <div className="form_input">
+                                            <input type="email" placeholder='Enter your email' ref={emailRef} />
+                                        </div>
+                                        {/* form_input */}
+                                        <div className="form_input">
+                                            <input type="text" placeholder='Enter subject' ref={subjectRef} />
+                                        </div>
+                                        {/* form_input */}
+                                        <div className="form_input">
+                                            <textarea rows="7" placeholder='Write Message' ref={messageRef}></textarea>
+                                        </div>
+                                        {/* send_btn */}
+                                        <button className="send_btn" style={{ border: 'none', padding: '7px 25px', borderRadius: '5px', marginTop: '20px' }}>Send a Message</button>
+                                    </form>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+            </Fragment>
+        </Helmet>
     )
 }
 
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import CommonSection from '../components/UI/CommonSection'
+import Helmet from '../components/Utils/Helmet'
+import { Container, Row, Col } from 'reactstrap'
+import './styles/Wallet.css'
+
+const wallet_data = [
+    {
+        title: 'Bitcoin',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non enim eget metus placerat interdum vitae non erat.',
+        icon: 'ri-bit-coin-line'
+    },
+    {
+        title: 'Coinbase',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non enim eget metus placerat interdum vitae non erat.',
+        icon: 'ri-coin-line'
+    },
+    {
+        title: 'Metamask',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non enim eget metus placerat interdum vitae non erat.',
+        icon: 'ri-money-cny-circle-line'
+    },
+    {
+        title: 'Authereum',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non enim eget metus placerat interdum vitae non erat.',
+        icon: 'ri-bit-coin-line'
+    },
+
+]
+
 
 const Wallet = () => {
     return (
-        <div>
-            Wallet
-        </div>
+        /* title='Wallet' */
+        <Helmet title='Wallet'>
+            <Fragment>
+                {/* CommonSection */}
+                <CommonSection title='Connect Wallet' />
+                <section>
+                    {/* Container */}
+                    <Container>
+                        {/* Row */}
+                        <Row>
+                            {/* lg='12' */}
+                            <Col lg='12' className="mb-5 text-center">
+                                {/* w-50 m-auto */}
+                                <div className="w-50 m-auto">
+                                    <h3 className="text-light">Connect your wallet</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non enim eget metus placerat interdum vitae non erat. Interdum et.
+                                    </p>
+                                </div>
+                            </Col>
+                            {
+                                wallet_data.map((item, index) => {
+                                    return (
+                                        <Col key={index} className="mb-4" lg='3' md='4' sm='6'>
+                                            {/* wallet_item */}
+                                            <div className="wallet_item">
+                                                {/* ri-bit-coin-line */}
+                                                <span>
+                                                    <i className={item.icon}></i>
+                                                </span>
+                                                <h5>{item.title}</h5>
+                                                {/* Sentence */}
+                                                <p>{item.desc}</p>
+                                            </div>
+                                        </Col>
+                                    )
+                                })
+                            }
+                        </Row>
+                    </Container>
+                </section>
+            </Fragment>
+        </Helmet>
     )
 }
 
